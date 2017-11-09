5e8299face6da913dab52acac7a74ac718c9f9d5
Adrian Göransson <adriangoransson@gmail.com>
2017-11-09 22:37:42 +0100

    Graceful restart

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"os/signal"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,5 +30,30 @@ func main() {
 
 	router.Static("/assets", "./assets")
 
-	router.Run() // listen and serve on 0.0.0.0:8080
+	// Handle server and shutdown. This is a copy-paste from the readme.
+	// https://github.com/gin-gonic/gin/blob/master/examples/graceful-shutdown/graceful-shutdown/server.go
+	srv := &http.Server{
+		Addr: 	":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
